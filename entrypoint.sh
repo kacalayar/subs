728347da8bfca1c849b1ac4607e4580e492ce931
David Maitland <david.maitland@sipcentric.com>
2020-03-17 20:44:36 +0000

    Changed DNS to use 9.9.9.9

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,7 +27,7 @@ if [ -z "${SUBSPACE_HTTP_INSECURE-}" ] ; then
     export SUBSPACE_HTTP_INSECURE="false"
 fi
 
-export NAMESERVER="1.1.1.1"
+export NAMESERVER="9.9.9.9"
 export DEBIAN_FRONTEND="noninteractive"
 
 # Set DNS server
